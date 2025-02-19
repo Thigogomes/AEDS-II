426ed08321e47da7e8db216e1e4d49f2d8c68443
Thiago Henrique Gomes Feliciano <thigomeslui@gmail.com>
2025-02-19 17:35:18 -0300

    Add files via upload

---
+++ TI-2-Exercicios/ti2-exercicios/exercicios/ti2-exercicios/src/main/java/ti2_exercicios/ti2_exercicios/App.java
@@ -0,0 +1,10 @@
+package ti2_exercicios.ti2_exercicios;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
