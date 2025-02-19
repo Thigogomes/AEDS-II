02c24c3ee7cc26e09f04ef72ba0d0deea7c2cd09
Thiago Henrique Gomes Feliciano <thigomeslui@gmail.com>
2025-02-19 17:30:12 -0300

    Add files via upload

---
+++ TI-2-Exercicios/cursojava/ti2/App.java
@@ -0,0 +1,10 @@
+package comp_ti2_.ti2;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ TI-2-Exercicios/cursojava/ti2/DAO.java
@@ -0,0 +1,128 @@
+package comp_ti2_.ti2;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DAO {
+	protected Connection conexao;
+	
+	public DAO() {
+		conexao = null;
+	}
+	
+	public boolean conectar() {
+		String driverName = "org.postgresql.Driver";                    
+		String serverName = "localhost";
+		String mydatabase = "teste";
+		int porta = 5432;
+		String url = "jdbc:postgresql://" + serverName + ":" + porta +"/" + mydatabase;
+		String username = "postgres";
+		String password = "marcos";
+		boolean status = false;
+
+		try {
+			Class.forName(driverName);
+			conexao = DriverManager.getConnection(url, username, password);
+			status = (conexao == null);
+			System.out.println("Conexão efetuada com o postgres!");
+		} catch (ClassNotFoundException e) { 
+			System.err.println("Conexão NÃO efetuada com o postgres -- Driver não encontrado -- " + e.getMessage());
+		} catch (SQLException e) {
+			System.err.println("Conexão NÃO efetuada com o postgres -- " + e.getMessage());
+		}
+
+		return status;
+	}
+	
+	public boolean close() {
+		boolean status = false;
+		
+		try {
+			conexao.close();
+			status = true;
+		} catch (SQLException e) {
+			System.err.println(e.getMessage());
+		}
+		return status;
+	}
+	
+	
+	public static String toMD5(String senha) throws Exception {
+		MessageDigest m= MessageDigest.getInstance("MD5");
+		m.update(senha.getBytes(),0, senha.length());
+		return new BigInteger(1,m.digest()).toString(16);
+	}
+
+	public boolean inserirUsuario(Usuario usuario) {
+		boolean status = false;
+		try {  
+			Statement st = conexao.createStatement();
+			String sql = "INSERT INTO usuario (codigo, login, senha, sexo) "
+				       + "VALUES ("+usuario.getCodigo()+ ", '" + usuario.getLogin() + "', '"  
+				       + usuario.getSenha() + "', '" + usuario.getSexo() + "');";
+			System.out.println(sql);
+			st.executeUpdate(sql);
+			st.close();
+			status = true;
+		} catch (SQLException u) {  
+			throw new RuntimeException(u);
+		}
+		return status;
+	}
+	
+	public boolean delete(int codigo) {
+		boolean status = false;
+		try {  
+			Statement st = conexao.createStatement();
+			String sql = "DELETE FROM usuario WHERE codigo = " + codigo;
+			System.out.println(sql);
+			st.executeUpdate(sql);
+			st.close();
+			status = true;
+		} catch (SQLException u) {  
+			throw new RuntimeException(u);
+		}
+		return status;
+	}
+	
+	public boolean update(Usuario usuario) {
+		boolean status = false;
+		try {  
+			Statement st = conexao.createStatement();
+			String sql = "UPDATE usuario SET login = '" + usuario.getLogin() + "', senha = '"  
+				       + usuario.getSenha() + "', sexo = '" + usuario.getSexo() + "'"
+					   + " WHERE codigo = " + usuario.getCodigo();
+			System.out.println(sql);
+			st.executeUpdate(sql);
+			st.close();
+			status = true;
+		} catch (SQLException u) {  
+			throw new RuntimeException(u);
+		}
+		return status;
+	}
+	
+	public Usuario[] getUsuarios() {
+		Usuario[] usuarios = null;
+		
+		try {
+			Statement st = conexao.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
+			ResultSet rs = st.executeQuery("SELECT * FROM usuario");	           
+	        if(rs.next()) {
+	        	rs.last();
+	        	usuarios = new Usuario[rs.getRow()];
+	        	rs.beforeFirst();
+	        	
+	        	for(int i = 0; rs.next(); i++) {
+	        		usuarios[i] = new Usuario(rs.getInt("codigo"), rs.getString("login"), rs.getString("senha"), rs.getString("sexo").charAt(0));
+	        	}
+	        }
+	        st.close();
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
+		return usuarios;
+	}
+
+}
