4ac3d37cdfc782a4c75985f11937b3efa4a1a067
Thiago Gomes <thigomeslui@gmail.com>
2025-03-28 09:28:12 -0300

    Add files via upload

---
+++ TI-2-Exercicios/spark.01/src/main/java/concursosService/ConcursosService.java
@@ -0,0 +1,82 @@
+package concursosService;
+
+import spark.Request;
+import spark.Response;
+import dao.ConcursoDAO;
+import concursos.Concurso;
+
+public class ConcursosService {
+	
+	public ConcursoDAO concursoDAO = new ConcursoDAO();
+	
+	public String insert(Request request, Response response) {
+		int codigo = Integer.parseInt(request.queryParams("codigo"));
+		String nome = request.queryParams("nome");
+		String categoria = request.queryParams("categoria");
+		String banca = request.queryParams("banca");
+		String localizacao = request.queryParams("localizacao");
+		String resp = "";
+		
+		Concurso concurso = new Concurso(codigo, nome, categoria, banca, localizacao);
+		
+		if(concursoDAO.insert(concurso)) {
+            resp = "Concurso (" + nome + ") inserido!";
+            response.status(201); 
+		} else {
+			resp = "Concurso (" + nome + ") não inserido!";
+			response.status(404);
+		}
+		return resp;
+	}
+	
+	public Object get(Request request, Response response) {
+		int id = Integer.parseInt(request.params(":id"));
+		Concurso concurso = concursoDAO.getConcurso(id);
+		
+		if(concurso != null) {
+			response.header("Content-Type", "application/xml");
+			response.header("Content-Encoding", "UTF-8");
+			
+			return "<concurso>\n" +
+					"\t<id>" + concurso.getCodigoConcurso() + "</id>\n" +
+					"\t<nome>" + concurso.getNomeConcurso() + "</nome>\n" +
+					"\t<categoria>" + concurso.getCategoria() + "</categoria>\n" +
+					"\t<banca>" + concurso.getBanca() + "</banca>\n" +
+					"\t<localizacao>" + concurso.getLocalizacao() + "</localizacao>\n" +
+					"</concurso>\n";
+		}else {
+			response.status(404);
+			return "Concurso " + id + " não encontrado";
+		}
+	}
+
+	public Object delete(Request request, Response response) {
+		int id = Integer.parseInt(request.params(":id"));
+
+		Concurso concurso = concursoDAO.getConcurso(id);
+		if(concurso != null) {
+			concursoDAO.delete(id);
+			response.status(200);
+			return "Concurso com ID " + id + " foi deletado com sucesso!";
+		}else {
+			response.status(404);
+			return "Concurso não encontrado!";
+		}
+	}
+	
+	public Object update(Request request, Response response) {
+		int id = Integer.parseInt(request.params(":id"));
+		Concurso concurso = concursoDAO.getConcurso(id);
+		if(concurso != null) {
+			concurso.setNomeConcurso(request.queryParams("nome"));
+			concurso.setCategoria(request.queryParams("categoria"));
+			concurso.setBanca(request.queryParams("banca"));
+			concurso.setLocalizacao(request.queryParams("localizacao"));
+			concursoDAO.update(concurso);
+			return id;
+		}else {
+			response.status(404);
+			return "Produto não encontrado!";
+		}
+	}
+}
---
+++ TI-2-Exercicios/spark.01/src/main/java/dao/teste.java
@@ -0,0 +1,32 @@
+package dao;
+
+import java.util.Scanner;
+import dao.ConcursoDAO;
+import concursos.Concurso;
+
+public class teste {
+	/*public static void main(String[] args) {
+		int codigo;
+		String nome, categoria, banca, localizacao;
+		Scanner input = new Scanner(System.in);
+		ConcursoDAO dao = new ConcursoDAO();
+		dao.conectar();
+		
+		System.out.println("Digite o código: ");
+		codigo = input.nextInt();
+		input.nextLine();
+		System.out.println("Digite o login: ");
+		nome = input.nextLine();
+		System.out.println("Digite a senha: ");
+		categoria = input.nextLine();
+		System.out.println("Digite a senha: ");
+		banca = input.nextLine();
+		System.out.println("Digite a senha: ");
+		localizacao = input.nextLine();
+		Concurso concurso = new Concurso(codigo, nome, categoria, banca, localizacao);
+		dao.insert(concurso);
+		Concurso concurso = dao.getConcurso(codigo);
+		System.out.println(concurso.toString());
+	}*/
+	
+}
