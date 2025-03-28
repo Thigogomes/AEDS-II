d3a753ad51a61c941e6556e0627237b72416b2ec
Thiago Gomes <thigomeslui@gmail.com>
2025-03-28 01:50:34 -0300

    Add files via upload

---
+++ TI-2-Exercicios/exerc3/spark.01/src/main/java/app/Aplicacao.java
@@ -0,0 +1,19 @@
+package app;
+
+import static spark.Spark.*;
+
+import concursosService.ConcursosService;
+
+public class Aplicacao {
+	
+	private static ConcursosService concursosService = new ConcursosService();
+	
+	public static void main(String[] args) {
+		staticFiles.externalLocation("src/main/resources/public");
+		port(8076);
+		post("/concurso", (request, response) -> concursosService.insert(request, response));
+		get("/concurso/:id", (request, response) -> concursosService.delete(request, response));
+		get("/concurso/:id", (request, response) -> concursosService.get(request, response));
+		post("/produto/update/:id", (request, response) -> concursosService.update(request, response));
+	}
+}
